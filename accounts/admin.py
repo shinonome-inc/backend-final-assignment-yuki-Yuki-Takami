4e5bb76d6c35fd5f8a45365d7963e0dd090c6cbc
yuki00900 <t122089@ed.sus.ac.jp>
2023-01-24 22:19:49 +0900

    signup

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,10 +2,5 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-
-class CustomUserAdmin(UserAdmin):
-    pass
-
-
 User = get_user_model()
 admin.site.register(User)
