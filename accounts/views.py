57356fdcd190e17aa16b0cc840e458dd1825a067
yuki00900 <t122089@ed.sus.ac.jp>
2023-02-18 10:59:01 +0900

    profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.views import generic
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -25,16 +26,5 @@ class SignUpView(CreateView):
 
 
 class UserProfileView(LoginRequiredMixin, generic.DetailView):
-    model = User
-    template_name = "accounts/profile.html"
-    context_object_name = "user"
-    slug_field = "username"
-    slug_url_kwarg = "username"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        user = self.object
-        context["tweet_list"] = user.tweet_set.order_by("-created_at")
-
-        return context
+    def get(self, request, *args, **kwargs):
+        return render(request, "accounts/profile.html")
