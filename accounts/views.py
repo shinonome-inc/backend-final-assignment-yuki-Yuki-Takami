2edee1cb478429be56b22861b2907d4295d164fc
yuki00900 <t122089@ed.sus.ac.jp>
2023-04-23 15:26:31 +0900

    likes_listの移動

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 
-from tweets.models import Like, Tweet
+from tweets.models import Tweet
 
 from .forms import SignUpForm
 from .models import FriendShip
